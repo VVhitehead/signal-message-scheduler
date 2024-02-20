7a12f8be3c99cb57938f24d6b04fe3c57a741070
VVhitehead <zeitgeistpk@protonmail.com>
2024-02-20 03:56:08 +0100

    Add loading animation and README usage opts

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::println;
 use colored::Colorize;
+use spinners::{Spinner, Spinners};
 
 pub mod util;
 pub mod contact;
@@ -7,8 +8,11 @@ pub mod contact;
 
 fn main() {
     println!("    -- Message scheduler for {} Messenger --\n\n","Signal".bold().blue());
-    
+
+    let mut sp = Spinner::new(Spinners::Point, "Fetching contacts".into());   
     let contacts = util::get_contact_list().unwrap();
+    sp.stop();
+    print!("\x1b[2K\r");
     
     util::run_dialogue(contacts.to_string());
 }
